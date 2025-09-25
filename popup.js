8469bd2e3a39fb2e2451c154d37f58bb072fdac3
GetOffer.help <oliver@tiwenti.chat>
2025-09-25 22:14:36 +0800

    Revert "x"
    This reverts commit ad2ecfb935488e3652460ec8aa267f4fbede04bf.

--- popup.js
+++ popup.js
@@ -4,7 +4,6 @@ class PopupController {
     this.currentStatus = null;
     this.currentPage = 'home';
     this.i18nManager = new I18nManager();
-    this.userDictionaryUI = new UserDictionaryUI(this.i18nManager);
     this.dictSettings = {
       zh: true,
       en: true,
@@ -65,9 +64,6 @@ class PopupController {
     
     // 检查版本
     await this.checkVersion();
-    
-    // 初始化用户词典UI
-    await this.initUserDictionaryUI();
   }
 
   bindEvents() {
@@ -620,12 +616,7 @@ class PopupController {
     console.log('文本设置已重置');
   }
 
-  async initUserDictionaryUI() {
-    // 初始化用户词典UI管理器
-    if (this.userDictionaryUI) {
-      await this.userDictionaryUI.init();
-    }
-  }
+
 
   async checkVersion() {
     try {
