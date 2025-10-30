b87eb251120d93e01eeda8c49b4300ec1c56ddff
GetOffer.help <oliver@tiwenti.chat>
2025-10-30 12:43:47 +0800

    Revert "xx"
    This reverts commit f26bf88cddf98483539f339a23c10f8c22a4fa3d.

--- background.js
+++ background.js
@@ -501,9 +501,6 @@ function hideReviewLightTower() {
     // 清除徽章文本
     chrome.action.setBadgeText({ text: '' });
     
-    // 清除徽章背景色
-    chrome.action.setBadgeBackgroundColor({ color: [0, 0, 0, 0] }); // 透明背景
-    
     // 移除存储的灯塔数据
     chrome.storage.local.remove(['reviewLightTowerData', 'reviewLightTowerVisible']);
     
