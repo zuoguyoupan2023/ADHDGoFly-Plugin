4738dd63508dcfc6bd8568c670da0e4ae7a02905
zuoguyoupan <oliver@tiwenti.chat>
2026-07-20 16:21:24 +0800

    Reapply "33"
    This reverts commit 951f26757103ac0a9606bf17fbd6af6d9211c7eb.

--- popup.js
+++ popup.js
@@ -503,7 +503,9 @@ class PopupController {
         (async () => {
           const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
           if (!tabs[0]) return;
-          chrome.tabs.sendMessage(tabs[0].id, { action: 'showAiSettingPanel' });
+          chrome.tabs.sendMessage(tabs[0].id, { action: 'openTaixue', module: 'chat', contextSource: 'full_article' }, () => {
+            if (chrome.runtime.lastError) console.warn('无法打开太学：', chrome.runtime.lastError.message);
+          });
         })();
         break;
       case 'faq-btn':
